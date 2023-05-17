79c87e0827ab9b52176d6a18c02eb620b83ba69a
zohaibabdullah1 <zohaibabdullah86@gmail.com>
2023-05-17 15:21:09 +0500

    Login UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Form from './Login';
+// import CheckExample from './check';
 
 function App() {
   return (
     <div className="App">
      <Form/>
+     {/* <CheckExample/> */}
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,46 +1,71 @@
-import './App.css';
+import "./App.css";
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import './index.css';
-import {Form, FormGroup, Label, Input} from 'reactstrap';
+import "./index.css";
+import { FormGroup, Label, Input } from "reactstrap";
+import Form from "react-bootstrap/Form";
+import { useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Button from 'react-bootstrap/Button';
 // import Form from 'react-bootstrap/Form';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
 
 function LoginForm() {
-    return (
-      
-        <div className='login'>
-        <Form action='post'>
-        <h2 className="display-6">Login Page</h2>
-        <FormGroup className="position-relative">
-          <Label for="email">Email Address</Label>
-          <Input type='email' value="talhanonari@gmail.com" required/>
-        </FormGroup>
-        <FormGroup className="position-relative">
-          <Label for="password">Password</Label>
-          <Input type='password' value="syco321" required/>
-        </FormGroup>
+    // const [checked, setChecked] = useState(false);
+    const [radioValue, setRadioValue] = useState('1');
+    const radios = [
+        { name: 'Login', value: '1' },
+       
+        { name: 'Register', value: '3' },
+      ];
+  return (
+<>
+<div className="Toggle">
+      <ButtonGroup>
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant={idx % 2 ? 'outline-success' : 'outline-success'}
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </div>
+
+
     
 
-        <div key={`default-${type}`} className="mb-3">
-          <Form.Check 
-            type={type}
-            id={`default-${type}`}
-            label={`default ${type}`}
-          />
-        </div>
-      
-
-          <div className="d-grid">
-      {/* <Button variant='success' size='md' onClick=>Login</Button> */}
+    <div className="Login-Form">
+      <div className="login">
+        <Form action="post">
+          <h2 className="display-6">Login</h2>
+          <FormGroup className="position-relative">
+            <Label for="email">Email Address</Label>
+            <Input type="email" required />
+          </FormGroup>
+          <FormGroup className="position-relative">
+            <Label for="password">Password</Label>
+            <Input type="password" required />
+          </FormGroup>
+          <Form.Check aria-label="option 1" label="Remember me" />
+          <div className="d-grid gap-2">
+          <Button size="lg">Login</Button>
           </div>
-      </Form>
-        {/* <Outlet/> */}
-        
-       
+          <a className="Forgot-Pass" href="#">Lost your Password? </a>
+        </Form>
       </div>
-    );
-  }
-  
-  export default LoginForm;
+
+    </div></>
+  );
+}
+
+export default LoginForm;
+//   export default CheckExample;
