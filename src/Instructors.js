efd9f20f4fc489d33dae026b4065014d4727a7d5
zohaibabdullah1 <zohaibabdullah86@gmail.com>
2023-05-23 18:56:37 +0500

    revert

--- src/Instructors.js
+++ src/Instructors.js
@@ -8,6 +8,18 @@ import inst1 from "../src/Images/inst1.jpg";
 import InstComp from "./InstructorComp";
 
 function Instructor() {
+
+    const desc = `I am the founder of A Cloud Guru and an Amazon Web Services
+    community hero. I hold every associate certification and I am a
+    certified AWS Solutions Architect Professional. I am ex-Rackspace,
+    ex-Sungard & ex-Smart421. I have been working in the Cloud space
+    since it's very inception. I am not just another I.T. trainer. I
+    teach Cloud because I know it. Inside and out. I've worked as a
+    systems administrator, developer, solutions architect & managing
+    director/general manager for some of the biggest companies in
+    cloud. My courses are not just some other IT training course,
+    delivered by some IT trainer. My background is very hands on. I
+    live eat and breath cloud.`
   return (
     <>
       {<NavComp />}
@@ -19,7 +31,14 @@ function Instructor() {
       </div>
 
       <div className="aboutUs">
-        <InstComp img={inst1} />
+        <InstComp img={inst1} desc={desc} />
+      </div>
+      <div className="aboutUs">
+        <InstComp img={inst1} desc={desc} />
+      </div>
+
+      <div className="aboutUs">
+        <InstComp img={inst1} desc={desc} />
       </div>
       <div>
         <Footer />
