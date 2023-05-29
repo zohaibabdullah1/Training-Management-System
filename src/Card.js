15c451dd6c712fc15a8295cf70ebd2d9abf27b50
talhanonari <asimriaz1998@gmail.com>
2023-05-29 20:26:45 +0500

    course in cards

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,29 @@
+import React, { useState} from "react";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 
-function Cards({img}){
+function Cards({course}){
   
     const navigate = useNavigate();
     function Enroll_Nav(){
       navigate("/enroll"); 
     }
+    const [image, setImage] = useState(course.image);
     
     return(
 
         <div >
       <Col>
           <Card className='cat-cards'>
-            <Card.Img className='cat-img' variant="top" src={img} />
+            <Card.Img className='cat-img' variant="top" src={image} alt={course.title}/>
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>{course.title}</Card.Title>
+              <Card.Title>{course.instructor}</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                {course.description}
               </Card.Text>
               <Button variant="dark " className='btn' onClick={Enroll_Nav}>Enrole Now</Button>
             </Card.Body>
